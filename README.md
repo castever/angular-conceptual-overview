b4d7d2db678f89f81969e92397c23c7689e2c2ee
Chad Stever <cstever@bluelid.com>
2014-10-21 07:10:39 -0600

    finished step 3

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ angular-overview-steps
 
 code from Angular JS Developer Guide Conceptual Overview (@see https://code.angularjs.org/1.2.26/docs/guide/concepts)
 
-second step in sequence on Conceptual Overview
+third step in sequence on Conceptual Overview
 
-adding a controller
+adding a service, dependency injection
--- invoice1.js
+++ invoice1.js
@@ -2,29 +2,18 @@
 
 // create a new angular module that matches the app name
 // see ng-app in index.html
-angular.module('invoice1', [])
+angular.module('invoice1', ['finance'])
   //create a new controller to "control" the app
-  .controller('InvoiceController', function() {
-    this.qty = 10;
+  .controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
+    this.qty = 1;
     this.cost = 2;
     this.inCurr = 'EUR';
-    this.currencies = ['USD', 'EUR', 'CNY'];
-    this.usdToForeignRates = {
-      USD: 1,
-      EUR: 0.74,
-      CNY: 6.09
-    };
+    this.currencies = currencyConverter.currencies;
 
     this.total = function total(outCurr) {
-      return this.convertCurrency(this.qty * this.cost,
-        this.inCurr, outCurr);
+      return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
     };
-
-    this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
-      return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
-    };
-
     this.pay = function pay() {
       window.alert("Thanks!");
     };
-  });
+  }]);
